17f13a59eb963453364d04be6c74e65cd7d324d3
big-billy <yusufabiola555@gmail.com>
2025-01-10 20:59:55 +0100

    my todolist

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 import Youtube from './project/youtube';
+import Todolist from './project/todolist';
 
 function App() {
   return (
     <>
-    <Youtube/>
+    <Todolist/>
+
 
 
     </>
